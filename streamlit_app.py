045310cff1755ee9f6ed4fbce322570c65714800
AxNorG <97640977+AxNorG@users.noreply.github.com>
2024-07-04 15:10:14 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,77 +6,76 @@ import io
 from utils import icon
 from streamlit_image_select import image_select
 
-# UI configurations
-st.set_page_config(page_title="Replicate Image Generator",
+# Настройки интерфейса
+st.set_page_config(page_title="Генератор копий изображений",
                    page_icon=":bridge_at_night:",
                    layout="wide")
 icon.show_icon(":foggy:")
 st.markdown("# :rainbow[Text-to-Image Artistry Studio]")
 
-# API Tokens and endpoints from `.streamlit/secrets.toml` file
+# Токены API и конечные точки из файла `.streamlit/secrets.toml`
 REPLICATE_API_TOKEN = st.secrets["REPLICATE_API_TOKEN"]
 REPLICATE_MODEL_ENDPOINTSTABILITY = st.secrets["REPLICATE_MODEL_ENDPOINTSTABILITY"]
 
-# Resources text, link, and logo
-replicate_text = "Stability AI SDXL Model on Replicate"
+# Текст ресурсов, ссылка и логотип
+replicate_text = "Стабильность модели AI SDXL на копиях"
 replicate_link = "https://replicate.com/stability-ai/sdxl"
 replicate_logo = "https://storage.googleapis.com/llama2_release/Screen%20Shot%202023-07-21%20at%2012.34.05%20PM.png"
 
-# Placeholders for images and gallery
+# Заполнители для изображений и галереи
 generated_images_placeholder = st.empty()
 gallery_placeholder = st.empty()
 
-
 def configure_sidebar() -> None:
     """
-    Setup and display the sidebar elements.
+    Настройка и отображение элементов боковой панели.
 
-    This function configures the sidebar of the Streamlit application, 
-    including the form for user inputs and the resources section.
+    Эта функция настраивает боковую панель приложения Streamlit,
+    включая форму для ввода данных пользователем и раздел ресурсов.
     """
     with st.sidebar:
         with st.form("my_form"):
-            st.info("**Yo fam! Start here ↓**", icon="👋🏾")
-            with st.expander(":rainbow[**Refine your output here**]"):
-                # Advanced Settings (for the curious minds!)
-                width = st.number_input("Width of output image", value=1024)
-                height = st.number_input("Height of output image", value=1024)
+            st.info("**Привет, начинайте здесь ↓**", icon="👋🏾")
+            with st.expander(":rainbow[**Уточните результат здесь**]"):
+                # Расширенные настройки (для любопытных!)
+                width = st.number_input("Ширина выходного изображения", value=1024)
+                height = st.number_input("Высота выходного изображения", value=1024)
                 num_outputs = st.slider(
-                    "Number of images to output", value=1, min_value=1, max_value=4)
-                scheduler = st.selectbox('Scheduler', ('DDIM', 'DPMSolverMultistep', 'HeunDiscrete',
-                                                       'KarrasDPM', 'K_EULER_ANCESTRAL', 'K_EULER', 'PNDM'))
+                    "Количество выходных изображений", value=1, min_value=1, max_value=4)
+                scheduler = st.selectbox('Планировщик', ('DDIM', 'DPMSolverMultistep', 'HeunDiscrete',
+                                                         'KarrasDPM', 'K_EULER_ANCESTRAL', 'K_EULER', 'PNDM'))
                 num_inference_steps = st.slider(
-                    "Number of denoising steps", value=50, min_value=1, max_value=500)
+                    "Количество шагов денойзинга", value=50, min_value=1, max_value=500)
                 guidance_scale = st.slider(
-                    "Scale for classifier-free guidance", value=7.5, min_value=1.0, max_value=50.0, step=0.1)
+                    "Масштаб для классификатор-независимого руководства", value=7.5, min_value=1.0, max_value=50.0, step=0.1)
                 prompt_strength = st.slider(
-                    "Prompt strength when using img2img/inpaint(1.0 corresponds to full destruction of infomation in image)", value=0.8, max_value=1.0, step=0.1)
+                    "Сила подсказки при использовании img2img/inpaint (1.0 соответствует полной деструкции информации в изображении)", value=0.8, max_value=1.0, step=0.1)
                 refine = st.selectbox(
-                    "Select refine style to use (left out the other 2)", ("expert_ensemble_refiner", "None"))
+                    "Выберите стиль уточнения (остались только 2)", ("expert_ensemble_refiner", "None"))
                 high_noise_frac = st.slider(
-                    "Fraction of noise to use for `expert_ensemble_refiner`", value=0.8, max_value=1.0, step=0.1)
+                    "Доля шума для `expert_ensemble_refiner`", value=0.8, max_value=1.0, step=0.1)
             prompt = st.text_area(
-                ":orange[**Enter prompt: start typing, Shakespeare ✍🏾**]",
-                value="An astronaut riding a rainbow unicorn, cinematic, dramatic")
-            negative_prompt = st.text_area(":orange[**Party poopers you don't want in image? 🙅🏽‍♂️**]",
-                                           value="the absolute worst quality, distorted features",
-                                           help="This is a negative prompt, basically type what you don't want to see in the generated image")
+                ":orange[**Введите подсказку: начните печатать, Шекспир ✍🏾**]",
+                value="Космонавт, едущий на радужном единороге, кинематографично, драматично")
+            negative_prompt = st.text_area(":orange[**Что не хотите видеть на изображении? 🙅🏽‍♂️**]",
+                                           value="ужаснейшее качество, искаженные черты",
+                                           help="Это отрицательная подсказка, укажите, что не хотите видеть на созданном изображении")
 
-            # The Big Red "Submit" Button!
+            # Большая красная кнопка "Отправить"!
             submitted = st.form_submit_button(
-                "Submit", type="primary", use_container_width=True)
+                "Отправить", type="primary", use_container_width=True)
 
-        # Credits and resources
+        # Кредиты и ресурсы
         st.divider()
         st.markdown(
-            ":orange[**Resources:**]  \n"
+            ":orange[**Ресурсы:**]  \n"
             f"<img src='{replicate_logo}' style='height: 1em'> [{replicate_text}]({replicate_link})",
             unsafe_allow_html=True
         )
         st.markdown(
             """
             ---
-            Follow me on:
+            Следите за мной:
 
             𝕏 → [@tonykipkemboi](https://twitter.com/tonykipkemboi)
 
@@ -87,37 +86,36 @@ def configure_sidebar() -> None:
 
         return submitted, width, height, num_outputs, scheduler, num_inference_steps, guidance_scale, prompt_strength, refine, high_noise_frac, prompt, negative_prompt
 
-
 def main_page(submitted: bool, width: int, height: int, num_outputs: int,
               scheduler: str, num_inference_steps: int, guidance_scale: float,
               prompt_strength: float, refine: str, high_noise_frac: float,
               prompt: str, negative_prompt: str) -> None:
-    """Main page layout and logic for generating images.
+    """Макет главной страницы и логика создания изображений.
 
     Args:
-        submitted (bool): Flag indicating whether the form has been submitted.
-        width (int): Width of the output image.
-        height (int): Height of the output image.
-        num_outputs (int): Number of images to output.
-        scheduler (str): Scheduler type for the model.
-        num_inference_steps (int): Number of denoising steps.
-        guidance_scale (float): Scale for classifier-free guidance.
-        prompt_strength (float): Prompt strength when using img2img/inpaint.
-        refine (str): Refine style to use.
-        high_noise_frac (float): Fraction of noise to use for `expert_ensemble_refiner`.
-        prompt (str): Text prompt for the image generation.
-        negative_prompt (str): Text prompt for elements to avoid in the image.
+        submitted (bool): Флаг, указывающий, была ли отправлена форма.
+        width (int): Ширина выходного изображения.
+        height (int): Высота выходного изображения.
+        num_outputs (int): Количество выходных изображений.
+        scheduler (str): Тип планировщика для модели.
+        num_inference_steps (int): Количество шагов денойзинга.
+        guidance_scale (float): Масштаб для классификатор-независимого руководства.
+        prompt_strength (float): Сила подсказки при использовании img2img/inpaint.
+        refine (str): Стиль уточнения для использования.
+        high_noise_frac (float): Доля шума для `expert_ensemble_refiner`.
+        prompt (str): Текстовая подсказка для создания изображения.
+        negative_prompt (str): Текстовая подсказка для элементов, которых нужно избегать в изображении.
     """
     if submitted:
-        with st.status('👩🏾‍🍳 Whipping up your words into art...', expanded=True) as status:
-            st.write("⚙️ Model initiated")
-            st.write("🙆‍♀️ Stand up and strecth in the meantime")
+        with st.status('👩🏾‍🍳 Создаем ваше искусство из слов...', expanded=True) as status:
+            st.write("⚙️ Модель запущена")
+            st.write("🙆‍♀️ Потянитесь пока что")
             try:
-                # Only call the API if the "Submit" button was pressed
+                # Вызов API только при нажатии кнопки "Отправить"
                 if submitted:
-                    # Calling the replicate API to get the image
+                    # Вызов API replicate для получения изображения
                     with generated_images_placeholder.container():
-                        all_images = []  # List to store all generated images
+                        all_images = []  # Список для хранения всех созданных изображений
                         output = replicate.run(
                             REPLICATE_MODEL_ENDPOINTSTABILITY,
                             input={
@@ -135,83 +133,81 @@ def main_page(submitted: bool, width: int, height: int, num_outputs: int,
                         )
                         if output:
                             st.toast(
-                                'Your image has been generated!', icon='😍')
-                            # Save generated image to session state
+                                'Ваше изображение создано!', icon='😍')
+                            # Сохранение созданного изображения в состоянии сессии
                             st.session_state.generated_image = output
 
-                            # Displaying the image
+                            # Отображение изображения
                             for image in st.session_state.generated_image:
                                 with st.container():
-                                    st.image(image, caption="Generated Image 🎈",
+                                    st.image(image, caption="Созданное изображение 🎈",
                                              use_column_width=True)
-                                    # Add image to the list
+                                    # Добавление изображения в список
                                     all_images.append(image)
 
                                     response = requests.get(image)
-                        # Save all generated images to session state
+                        # Сохранение всех созданных изображений в состоянии сессии
                         st.session_state.all_images = all_images
 
-                        # Create a BytesIO object
+                        # Создание объекта BytesIO
                         zip_io = io.BytesIO()
 
-                        # Download option for each image
+                        # Опция скачивания для каждого изображения
                         with zipfile.ZipFile(zip_io, 'w') as zipf:
                             for i, image in enumerate(st.session_state.all_images):
                                 response = requests.get(image)
                                 if response.status_code == 200:
                                     image_data = response.content
-                                    # Write each image to the zip file with a name
+                                    # Запись каждого изображения в zip файл с именем
                                     zipf.writestr(
                                         f"output_file_{i+1}.png", image_data)
                                 else:
                                     st.error(
-                                        f"Failed to fetch image {i+1} from {image}. Error code: {response.status_code}", icon="🚨")
-                        # Create a download button for the zip file
+                                        f"Не удалось получить изображение {i+1} с {image}. Код ошибки: {response.status_code}", icon="🚨")
+                        # Создание кнопки скачивания для zip файла
                         st.download_button(
-                            ":red[**Download All Images**]", data=zip_io.getvalue(), file_name="output_files.zip", mime="application/zip", use_container_width=True)
-                status.update(label="✅ Images generated!",
+                            ":red[**Скачать все изображения**]", data=zip_io.getvalue(), file_name="output_files.zip", mime="application/zip", use_container_width=True)
+                status.update(label="✅ Изображения созданы!",
                               state="complete", expanded=False)
             except Exception as e:
                 print(e)
-                st.error(f'Encountered an error: {e}', icon="🚨")
+                st.error(f'Возникла ошибка: {e}', icon="🚨")
 
-    # If not submitted, chill here 🍹
+    # Если не отправлено, остаемся здесь 🍹
     else:
         pass
 
-    # Gallery display for inspo
+    # Отображение галереи для вдохновения
     with gallery_placeholder.container():
         img = image_select(
-            label="Like what you see? Right-click and save! It's not stealing if we're sharing! 😉",
+            label="Нравится то, что видите? Сохраните изображение, если мы его поделились! 😉",
             images=[
                 "gallery/farmer_sunset.png", "gallery/astro_on_unicorn.png",
                 "gallery/friends.png", "gallery/wizard.png", "gallery/puppy.png",
                 "gallery/cheetah.png", "gallery/viking.png",
             ],
-            captions=["A farmer tilling a farm with a tractor during sunset, cinematic, dramatic",
-                      "An astronaut riding a rainbow unicorn, cinematic, dramatic",
-                      "A group of friends laughing and dancing at a music festival, joyful atmosphere, 35mm film photography",
-                      "A wizard casting a spell, intense magical energy glowing from his hands, extremely detailed fantasy illustration",
-                      "A cute puppy playing in a field of flowers, shallow depth of field, Canon photography",
-                      "A cheetah mother nurses her cubs in the tall grass of the Serengeti. The early morning sun beams down through the grass. National Geographic photography by Frans Lanting",
-                      "A close-up portrait of a bearded viking warrior in a horned helmet. He stares intensely into the distance while holding a battle axe. Dramatic mood lighting, digital oil painting",
+            captions=["Фермер, работающий на тракторе на закате, кинематографично, драматично",
+                      "Космонавт, едущий на радужном единороге, кинематографично, драматично",
+                      "Группа друзей, смеющихся и танцующих на музыкальном фестивале, радостная атмосфера, фотография на пленку 35 мм",
+                      "Волшебник, произносящий заклинание, из его рук исходит интенсивная магическая энергия, крайне детализированная фантастическая иллюстрация",
+                      "Милый щенок, играющий на поле с цветами, малая глубина резкости, фотография на Canon",
+                      "Мать-гепард кормит своих детенышей в высокой траве Серенгети. Утреннее солнце проникает через траву. Фотография National Geographic от Франса Лантина",
+                      "Крупный портрет бородатого викинга в рогатом шлеме. Он смотрит вдаль, держа боевой топор. Драматическое освещение, цифровая масляная живопись",
                       ],
             use_container_width=True
         )
 
-
 def main():
     """
-    Main function to run the Streamlit application.
+    Основная функция для запуска приложения Streamlit.
 
-    This function initializes the sidebar configuration and the main page layout.
-    It retrieves the user inputs from the sidebar, and passes them to the main page function.
-    The main page function then generates images based on these inputs.
+    Эта функция инициализирует конфигурацию боковой панели и макет главной страницы.
+    Она получает пользовательские данные с боковой панели и передает их в основную функцию страницы.
+    Основная функция страницы затем создает изображения на основе этих данных.
     """
     submitted, width, height, num_outputs, scheduler, num_inference_steps, guidance_scale, prompt_strength, refine, high_noise_frac, prompt, negative_prompt = configure_sidebar()
     main_page(submitted, width, height, num_outputs, scheduler, num_inference_steps,
               guidance_scale, prompt_strength, refine, high_noise_frac, prompt, negative_prompt)
-
 
 if __name__ == "__main__":
     main()
